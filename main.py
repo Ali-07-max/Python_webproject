ebace0573e9e4807713e901bb66f312eb84bca3e
Ali-07-max <142290935+Ali-07-max@users.noreply.github.com>
2024-08-01 02:21:34 +0000

    file upload is done

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
 from flask.helpers import redirect
 import os
 
 app = Flask(__name__, template_folder='templets', static_folder='statics')
+app.config['upload_folder'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'statics', 'uploads')
 
 
 @app.route('/')
 @app.route('/home')
 def index():
+  print(app.config['upload_folder'])
   return render_template('index.html')
 
 
@@ -15,7 +18,8 @@ def index():
 def upload():
   # Get the uploaded file
   file = request.files['file']
-  file.save(f'/uploads/{file.filename}')
+  if file:
+    file.save(os.path.join(app.config['upload_folder'], secure_filename(file.filename)))
   return redirect('/')
 
 
