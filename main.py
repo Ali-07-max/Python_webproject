d4c65199fd65c266ba4841bac713ddef33226f7e
Ali-07-max <142290935+Ali-07-max@users.noreply.github.com>
2024-08-01 03:34:53 +0000

    converting image to pdf

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, send_file
+from os.path import splitext
+from re import split
+from flask import Flask, config, render_template, request, send_file
 from werkzeug.utils import secure_filename
 from flask.helpers import redirect
 import os
 from werkzeug.exceptions import RequestEntityTooLarge
+from PIL import Image , ImageEnhance
 
 app = Flask(__name__, template_folder='templets', static_folder='statics')
-app.config['upload_folder'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'statics', 'uploads')
+app.config['upload_folder'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
+                                           'statics', 'uploads')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16MB
 app.config['ALLOWED_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif']
-
+app.config['PDF_FOLDER'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
+                                        'statics' , 'pdfs')
 @app.route('/')
 @app.route('/home')
 def index():
@@ -26,6 +31,10 @@ def upload():
       if extension not in app.config['ALLOWED_EXTENSIONS']:
         return 'File is not an image.'
       file.save(os.path.join(app.config['upload_folder'], secure_filename(file.filename)))
+      image = Image.open(os.path.join(app.config['upload_folder'], secure_filename(file.filename)))
+      nameoffile = os.path.splitext(file.filename)[0]               
+      pdf_path = os.path.join(app.config['PDF_FOLDER'], f"{nameoffile}.pdf")
+      image.save(pdf_path, "PDF")
     return redirect('/')
   except  RequestEntityTooLarge:
     return 'File is larger then 16MBs.'
